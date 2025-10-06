0ed02be803ba7d4a852847627106c1df756670e5
MiguelBad <badayosbiboy@gmail.com>
2025-10-06 22:12:41 +0930

    added account points and get account status in users model

--- EasyGames/Models/User.cs
+++ EasyGames/Models/User.cs
@@ -4,6 +4,23 @@ namespace EasyGames.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    public ICollection<Order> Orders {get; set;} = new List<Order>();
-    public ICollection<Review> Reviews {get; set;} = new List<Review>();
+    public int AccountPoints { get; set; }
+
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+    public const int BronzePoints = 500;
+    public const int SilverPoints = 1_000;
+    public const int GoldPoints = 5_000;
+    public const int PlatinumPoints = 10_000;
+
+    public string AccountStatus
+    {
+        get { 
+            if (AccountPoints >= PlatinumPoints) return "Platinum";
+            if (AccountPoints >= GoldPoints) return "Gold";
+            if (AccountPoints >= SilverPoints) return "Silver";
+            return "Bronze";
+        }
+    }
 }
