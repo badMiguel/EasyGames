2b24c98a47d86c39e6131f8973911b36464ec216
miguel <badayosbiboy@gmail.com>
2025-10-07 13:09:24 +0930

    generated crud scaffold for shop (unedited)

---
+++ EasyGames/Controllers/ShopController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyGames.Data;
+using EasyGames.Data;
+using EasyGames.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyGames.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly EasyGamesContext _context;
+
+        public ShopController(EasyGamesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shop
+        public async Task<IActionResult> Index()
+        {
+            var easyGamesContext = _context.Shop.Include(s => s.Owner);
+            return View(await easyGamesContext.ToListAsync());
+        }
+
+        // GET: Shop/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context
+                .Shop.Include(s => s.Owner)
+                .FirstOrDefaultAsync(m => m.ShopId == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return View(shop);
+        }
+
+        // GET: Shop/Create
+        public IActionResult Create()
+        {
+            ViewData["OwnerId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Shop/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("ShopId,ShopName,ContactNumber,LocationType,Address,OwnerId")] Shop shop
+        )
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shop);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OwnerId"] = new SelectList(_context.Users, "Id", "Id", shop.OwnerId);
+            return View(shop);
+        }
+
+        // GET: Shop/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shop.FindAsync(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            ViewData["OwnerId"] = new SelectList(_context.Users, "Id", "Id", shop.OwnerId);
+            return View(shop);
+        }
+
+        // POST: Shop/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(
+            int id,
+            [Bind("ShopId,ShopName,ContactNumber,LocationType,Address,OwnerId")] Shop shop
+        )
+        {
+            if (id != shop.ShopId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shop);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShopExists(shop.ShopId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OwnerId"] = new SelectList(_context.Users, "Id", "Id", shop.OwnerId);
+            return View(shop);
+        }
+
+        // GET: Shop/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context
+                .Shop.Include(s => s.Owner)
+                .FirstOrDefaultAsync(m => m.ShopId == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return View(shop);
+        }
+
+        // POST: Shop/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shop = await _context.Shop.FindAsync(id);
+            if (shop != null)
+            {
+                _context.Shop.Remove(shop);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShopExists(int id)
+        {
+            return _context.Shop.Any(e => e.ShopId == id);
+        }
+    }
+}
