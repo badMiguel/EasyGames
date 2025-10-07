281d11babc8df68d2a176fbe8c8c971c7a19ddb4
miguel <badayosbiboy@gmail.com>
2025-10-07 14:59:31 +0930

    updated display name for shop models

--- EasyGames/Models/Shop.cs
+++ EasyGames/Models/Shop.cs
@@ -13,19 +13,23 @@ public class Shop
     public int ShopId { get; set; }
 
     [Required]
+    [Display(Name = "Shop Name")]
     public string? ShopName { get; set; }
 
     [Phone]
     [Required]
     [MaxLength(20)]
+    [Display(Name = "Contact Number")]
     public string? ContactNumber { get; set; }
 
+    [Display(Name = "Location Type")]
     public LocationTypes LocationType { get; set; } = LocationTypes.Physical;
 
     // Physical stores should have address, make sure to add validation
     public string? Address { get; set; }
 
     // Foreign key User (ASP.Net Core Identity)
+    [Display(Name = "Owner ID")]
     public string? OwnerId { get; set; }
     public ApplicationUser? Owner { get; set; }
 
